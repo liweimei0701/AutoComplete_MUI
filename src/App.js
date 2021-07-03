148587c573505cd7cd9e7052e0d538984560330b
Ashley <liweimei0701@gmail.com>
2021-07-03 17:00:18 +1000

    try AutoComplete component in MUI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import SelectBox from './mui';
+import CountrySelect from "./mui2";
+import SelevtDrop from "./mui3";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div style={{width:500,margin:"300px auto"}}>
+        {/* <SelectBox></SelectBox> */}
+        <CountrySelect></CountrySelect>
+        {/* <SelevtDrop></SelevtDrop> */}
+        </div>
+      
     </div>
+    
+   
   );
 }
 
---
+++ src/mui4.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+const options = ['Option 1', 'Option 2'];
+
+export default function CustomInputAutocomplete() {
+  return (
+    <Autocomplete
+      id="custom-input-demo"
+      options={options}
+      renderInput={(params) => (
+        <div ref={params.InputProps.ref}>
+          <select></select>
+          <div>+61</div>
+          <input style={{ width: 200 }} type="text" {...params.inputProps} />
+        </div>
+      )}
+    />
+  );
+}
